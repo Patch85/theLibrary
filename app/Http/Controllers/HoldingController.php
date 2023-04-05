41b7f9652080a89820ff90f72ce1c23001a30ee0
Dillon A. John <dillon.a.john@gmail.com>
2023-04-05 07:28:02 -0400

    0.6.0 - Holdings
    - issue18
    - Add generated Model, Controller, Form Request classes, Policy,
      Factory, Migration and Seeder for Holdings
    - Holdings represent items in a user's Collection

---
+++ app/Http/Controllers/HoldingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreHoldingRequest;
+use App\Http\Requests\UpdateHoldingRequest;
+use App\Models\Holding;
+
+class HoldingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHoldingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Holding $holding)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Holding $holding)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHoldingRequest $request, Holding $holding)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Holding $holding)
+    {
+        //
+    }
+}
