3f274ea7e7331f0a340f071c0b3914c81b2d199f
Dillon A. John <dillon.a.john@gmail.com>
2023-04-09 20:54:31 -0400

    0.6.1 - Holding - Basic Definiitions
    - Define the Holding model, factory, seeder and migration classes as a
      basic template for different holding types.
    - Add the HoldingSeeder to DatabaseSeeder
    - Define the One to Many relationship between User and Holding in the
      User model

--- app/Models/Holding.php
+++ app/Models/Holding.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Holding extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'holding_type'
+    ];
+
+    /**
+     * A holding belongs to one user at any given time
+     * 
+     * @return BelongsTo 
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/factories/HoldingFactory.php
+++ database/factories/HoldingFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class HoldingFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::all()->random()->id,
+            'name' => fake()->words(3, true),
+            'description' => fake()->sentence(),
+            'holding_type' => fake()->randomElement(['book', 'eBook', 'article', 'DVD', 'CD']),
         ];
     }
 }
