11ff598ae28cfa7d108dc0ec919dedced88bf382
Dillon A. John <dillon.a.john@gmail.com>
2023-04-10 07:42:49 -0400

    0.7.1 - Collection: Model & Migration
    - Defined the basic Collection model and migration classes

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Collection extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * A Collection belongs to one user
+     * 
+     * @return BelongsTo 
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * A Collection can contain many Holdings
+     * 
+     * @return HasMany 
+     */
+    public function contents()
+    {
+        return $this->hasMany(Holding::class);
+    }
 }
